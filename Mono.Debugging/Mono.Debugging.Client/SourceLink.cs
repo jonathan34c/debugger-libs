e3842e6b3fc6d3d3ed963f1b1df2b93d2a3626c1
nosami <jasonimison@gmail.com>
2022-04-26 14:50:00 +0100

    stash

--- Mono.Debugging/Mono.Debugging.Client/SourceLink.cs
+++ Mono.Debugging/Mono.Debugging.Client/SourceLink.cs
@@ -1,23 +1,41 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Mono.Debugging.Client
 {
-	[Serializable]
-	public class SourceLink
-	{
-		public string Uri { get; }
-		public string RelativeFilePath { get; }
+    [Serializable]
+    public class SourceLink
+    {
+        public string Uri { get; }
+        public string RelativeFilePath { get; }
 
-		public SourceLink (string uri, string relativeFilePath)
-		{
-			RelativeFilePath = relativeFilePath;
-			Uri = uri;
-		}
+        public SourceLink (string uri, string relativeFilePath)
+        {
+            RelativeFilePath = relativeFilePath;
+            Uri = uri;
+        }
 
-		public string GetDownloadLocation (string cachePath)
-		{
-			return Path.Combine (cachePath, new Uri (Uri).GetComponents (UriComponents.Path, UriFormat.SafeUnescaped));
-		}
-	}
+        public string GetDownloadLocation (string cachePath)
+        {
+            return Path.Combine (cachePath, MakeValidFileName (Uri));
+        }
+
+        static string MakeValidFileName (string text)
+        {
+            var sb = new StringBuilder (text.Length);
+            var invalids = Path.GetInvalidFileNameChars ();
+            for (int i = 0; i < text.Length; i++) {
+                char c = text[i];
+                if (invalids.Contains (c)) {
+                    sb.Append ('_');
+                } else
+                    sb.Append (c);
+            }
+            if (sb.Length == 0)
+                return "_";
+            return sb.ToString ();
+        }
+    }
 }
